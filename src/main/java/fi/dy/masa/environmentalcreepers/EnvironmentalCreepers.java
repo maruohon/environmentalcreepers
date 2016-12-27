120fffa6dce6f5b34836ef46c8737c11af28e423
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2016-12-27 23:49:55 +0200

    Update to Minecraft 1.11.2. Update gradle stuff.

--- src/main/java/fi/dy/masa/environmentalcreepers/EnvironmentalCreepers.java
+++ src/main/java/fi/dy/masa/environmentalcreepers/EnvironmentalCreepers.java
@@ -12,7 +12,7 @@ import fi.dy.masa.environmentalcreepers.proxy.ServerProxy;
 @Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.MOD_VERSION,
     guiFactory = "fi.dy.masa.environmentalcreepers.config.EnvironmentalCreepersGuiFactory",
     updateJSON = "https://raw.githubusercontent.com/maruohon/environmentalcreepers/master/update.json",
-    acceptableRemoteVersions = "*", acceptedMinecraftVersions = "1.11")
+    acceptableRemoteVersions = "*", acceptedMinecraftVersions = "[1.11,1.11.2]")
 public class EnvironmentalCreepers
 {
     @Mod.Instance(Reference.MOD_ID)
