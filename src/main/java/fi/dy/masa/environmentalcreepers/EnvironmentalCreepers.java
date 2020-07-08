cf515f7b4f0df03f8c295856197b9fd1e86913ac
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2020-07-08 19:05:40 +0300

    Fix the warning about the mod missing from the clients

--- src/main/java/fi/dy/masa/environmentalcreepers/EnvironmentalCreepers.java
+++ src/main/java/fi/dy/masa/environmentalcreepers/EnvironmentalCreepers.java
@@ -1,10 +1,12 @@
 package fi.dy.masa.environmentalcreepers;
 
 import java.io.File;
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import net.minecraft.world.storage.FolderName;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.ExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
@@ -15,6 +17,7 @@ import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.event.server.FMLServerStoppingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLPaths;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
 import fi.dy.masa.environmentalcreepers.commands.CommandReloadConfig;
 import fi.dy.masa.environmentalcreepers.config.Configs;
 import fi.dy.masa.environmentalcreepers.event.ExplosionEventHandler;
@@ -30,6 +33,10 @@ public class EnvironmentalCreepers
 
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::onCommonSetup);
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::onFingerPrintViolation);
+
+        // Make sure the mod being absent on the other network side does not cause
+        // the client to display the server as incompatible
+        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (incoming, isNetwork) -> true));
 
         MinecraftForge.EVENT_BUS.register(new ExplosionEventHandler());
         MinecraftForge.EVENT_BUS.addListener(this::onServerAboutToStart);
